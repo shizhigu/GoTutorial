9b95e9e906621f87a9faf0ab782f56122baf08e7
Shizhi <60305156+shizhigu@users.noreply.github.com>
2020-04-16 04:32:33 -0700

    Add files via upload

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
 	"github.com/shizhigu/GoTutorial/sum"
 	"github.com/shizhigu/GoTutorial/handler"
 	"github.com/micro/go-micro"
+	//"github.com/micro/go-micro/api/proto/api.proto"
 )
 
 func main() {
 	srv := micro.NewService(
-		micro.Name("go.micro.srv.sum_service"),
+		micro.Name("go.micro.haha.srv.sum_service"),
 	)
 
 	srv.Init()
--- open-api/main.go
+++ open-api/main.go
@@ -9,6 +9,8 @@ import (
 	"strconv"
 	"encoding/json"
 	"github.com/micro/go-micro"
+	proto "github.com/shizhigu/GoTutorial/open-api/proto"
+	"fmt"
 
 )
 
@@ -20,9 +22,12 @@ type Open struct {
 
 }
 
-func (open Open) Fetch(ctx context.Context, req *api.Request, rsp *api.Response) error {
+func (open *Open) Fetch(ctx context.Context, req *api.Request, rsp *api.Response) error {
+	fmt.Println(req)
+	fmt.Println("hello")
 	sumInputStr := req.Get["sum"].Values[0]
-
+	fmt.Println(sumInputStr)
+	
 	sumInput, _ := strconv.ParseInt(sumInputStr, 10, 10)
 	// sumInput = int32(sumInput)
 	
@@ -30,14 +35,19 @@ func (open Open) Fetch(ctx context.Context, req *api.Request, rsp *api.Response)
 	sumReq := &sum.SumRequest{
 		Input: int32(sumInput),
 	}
+	
 
-
-	sumRsp, _ := srvClient.GetSum(ctx, sumReq)
-
+	sumRsp, err := srvClient.GetSum(context.Background(), sumReq)
+	if err != nil {
+		panic(err)
+	}
+	
+	fmt.Println(sumRsp.Output)
+	fmt.Println("world")
 	ret, _ := json.Marshal(map[string]interface{} {
-		"sum":	sumRsp,
+		"sum":	sumRsp.Output,
 	})
-
+	
 	rsp.Body = string(ret)
 	return nil
 }
@@ -46,12 +56,17 @@ func (open Open) Fetch(ctx context.Context, req *api.Request, rsp *api.Response)
 
 func main() {
 	service := micro.NewService(
-		micro.Name("go.micro.api.open"),
+		micro.Name("go.micro.haha.api.open"),
 	)
+	service.Init()
 
 	srvClient = sum.NewSumService("github.com/shizhigu/GoTutorial/sum", service.Client())
-	
-	if err:=service.Run(); err!=nil {
-		panic(err)
-	}
+
+	proto.RegisterOpenHandler(service.Server(), new(Open))
+	// service.HandleFunc("/sum", Sum)
+	fmt.Println("HIHIHIHI")
+	_ = service.Run()
+	// if err:=service.Run(); err!=nil {
+	// 	panic(err)
+	// }
 }
